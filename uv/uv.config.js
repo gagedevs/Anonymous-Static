73ae6cf8281841f593fa7ec35eb868254b4371c9
oHeckGage <68713542+oHeckGage@users.noreply.github.com>
2022-12-03 11:27:46 -0500

    made bare server shuttle bare
    changed from incog.dev bare server to the faster and less used shuttle.cf bare server 😮

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/uv/',
-    bare: 'https://incog.dev/bare/',
+    bare: 'https://shuttle.cf/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: "../uv/uv.handler.js",
